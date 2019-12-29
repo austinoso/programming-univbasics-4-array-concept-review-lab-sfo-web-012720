cd1bb8e249c9518950b79d7d70972002f2badeca
Austin Osorio <osorio_austin@yahoo.com>
2019-12-29 04:39:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -19,5 +19,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+lowest_num = 0
+
+  array.length.times { |index|
+    if array[index] > highestNum
+      highestNum = array[index]
+    end
+  }
+  highestNum
 end
