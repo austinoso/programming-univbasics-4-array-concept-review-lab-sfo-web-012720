7697d4de9654c6e3888964dd9f897dc81bc42cbc
Austin Osorio <osorio_austin@yahoo.com>
2019-12-29 04:29:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
 
   array.length.times { |index|
     if array[index] == value_to_find
-      return array[index]
+      return value_to_find
     end
   }
 
